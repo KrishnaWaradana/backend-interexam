53db6d5dcba9a58324272f9d4739d703e2c9a3a8
I Komang Adi Wirata <mangadiwirata578@gmail.com>
2026-01-22 18:01:31 +0800

    memperbaiki kategori dan membuat crud jenjang

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -9,12 +9,12 @@ const { authenticateToken, requireRole } = require('../middleware/authMiddleware
 router.get('/', authenticateToken, categoryController.getAllCategories);
 
 // POST - Hanya Admin
-router.post('/', authenticateToken, requireRole(['Admin']), categoryController.createCategory);
+router.post('/', authenticateToken, requireRole(['Admin', 'Validator']), categoryController.createCategory);
 
 // PUT - Hanya Admin
-router.put('/:id', authenticateToken, requireRole(['Admin']), categoryController.updateCategory);
+router.put('/:id', authenticateToken, requireRole(['Admin', 'Validator']), categoryController.updateCategory);
 
 // DELETE - Hanya Admin
-router.delete('/:id', authenticateToken, requireRole(['Admin']), categoryController.deleteCategory);
+router.delete('/:id', authenticateToken, requireRole(['Admin', 'Validator']), categoryController.deleteCategory);
 
 module.exports = router;
