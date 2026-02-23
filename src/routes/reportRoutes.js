6288e38dc0c71b31b2755b5258c6721b40fe7ac0
hendra123004 <hendraprawira751@gmail.com>
2026-02-24 00:07:55 +0800

    perbaikan lagi

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -5,15 +5,15 @@ const { authenticateToken } = require('../middleware/authMiddleware');
 
 // --- 1. VERSI UJI CUBA (Bypass ID 13) ---
 // Gunakan ini untuk tes di user_test.http tanpa login
-// const bypassAuth = (req, res, next) => {
-//     req.user = { id_user: 13, role: 'Admin' }; 
-//     next();
-// };
-// router.get('/stats', bypassAuth, getReportData);
+const bypassAuth = (req, res, next) => {
+    req.user = { id_user: 13, role: 'Admin' }; 
+    next();
+};
+router.get('/stats', bypassAuth, getReportData);
 
 
 // --- 2. VERSI RESMI (Untuk Frontend) ---
 // Hapus komentar di bawah ini dan hapus route bypass di atas jika sudah siap produksi
-router.get('/stats', authenticateToken, getReportData);
+// router.get('/stats', authenticateToken, getReportData);
 
 module.exports = router;
